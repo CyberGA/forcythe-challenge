5429e6d937f3b21d5c0a2670dd6a7220b654c752
CyberGA <etomu.joshua@gmail.com>
2025-01-13 12:56:45 +0100

    update: content

--- components/home/Blog.tsx
+++ components/home/Blog.tsx
@@ -41,7 +41,7 @@ export default function BlogSection() {
             words="Read our articles, news and product blog"
             filter={false}
           />
-          <CustomButton clx="w-fit" />
+          <CustomButton showIcon text="Visit Blog" clx="w-fit" />
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-12">
           {CONTENT.map((content, index) => (
@@ -59,13 +59,13 @@ export default function BlogSection() {
                 </div>
                 <div className="pl-5 relative before:absolute before:w-[1px] before:h-[90%] before:bg-white before:left-0 before:top-[50%] before:-translate-y-[50%] group-hover:translate-x-4 duration-300">
                   <p className="text-lg font-semibold mb-1">{content.title}</p>
-                  <div className="flex items-center text-base text-cGreyDark mb-6">
+                  <div className="flex items-center gap-1 text-base text-cGreyDark mb-6">
                     <span>{content.caption}</span>
                     <Circle className="size-2 fill-white" />
                     <span>May 29th, 2024</span>
                   </div>
                   <h6 className="text-xl md:text-2xl font-semibold line-clamp-2">
-                    Will AI take over Art?
+                    {content.desc}
                   </h6>
                 </div>
               </div>
